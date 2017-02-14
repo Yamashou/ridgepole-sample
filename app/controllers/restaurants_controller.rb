c257753a2acfada49507c403fc92c0a1e6acd60b
Shota Yamamota <1230124fw@gmail.com>
2017-02-14 21:12:14 +0900

    曜日判定

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,17 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurant.all
+    weekDay = ["sun","mon","tue","wed","thu","fri","sat"]
+    d = Time.zone.now
+    restaurants = Restaurant.all
+    # restaurants.push(Restaurant.first)
+    @op_restaurants = []
+    restaurants.each do |t|
+      s=t.operation
+      if s[weekDay[d.wday]]
+        @op_restaurants.push(t)
+      end
+    end
+
   end
 
   def show
