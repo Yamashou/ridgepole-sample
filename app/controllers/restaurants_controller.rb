b4b39a8421c8a816588d532443b8ef011c0311bd
Shota Yamamota <1230124fw@gmail.com>
2017-03-03 14:56:53 +0900

    表示店舗を休日店舗のカット

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,16 +2,8 @@ class RestaurantsController < ApplicationController
   def index
     weekDay = ["sun","mon","tue","wed","thu","fri","sat"]
     d = Time.zone.now
-    restaurants = Restaurant.all
-    # restaurants.push(Restaurant.first)
-    @op_restaurants = []
-    restaurants.each do |t|
-      s=t.operation
-      if s[weekDay[d.wday]]
-        @op_restaurants.push(t)
-      end
-    end
-
+    name = weekDay[d.wday] + " = true"
+    @resutaurants = Operation.where(name).map(&:restaurant)
   end
 
   def show
