b84f1fbe06891ec79f9fd775078d18c9da1d209e
Shota Yamamota <1230124fw@gmail.com>
2017-03-03 21:42:33 +0900

    pg

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,8 +2,10 @@ class RestaurantsController < ApplicationController
   def index
     weekDay = ["sun","mon","tue","wed","thu","fri","sat"]
     d = Time.zone.now
-    name = weekDay[d.wday] + " = true"
-    @resutaurants = Operation.where(name).map(&:restaurant)
+    name = weekDay[d.wday] + " = true + open_time <= ? and ? <= end_time"
+    nowTime = Time.new(2000,1,1,d.strftime("%I"),d.strftime("%M"))
+    s = Operation.where(name).map(&:restaurant)
+    @resutaurants = Operation.where(name,nowTimem,nowTimem).map(&:restaurant)
   end
 
   def show
